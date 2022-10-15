dbb80aa721cf183e9d58dc5b0365161415cada29
Danil-Zhikharev <Dane99mail.rugit config --global user.name Danil-Zhikharev$  git config --global user.email Dane99mail.ru>
2022-10-15 20:26:42 +0500

    Last_commit_20:26

---
+++ src/test/java/ru/netology/sqr/SQRServiceTest_2.java
@@ -0,0 +1,14 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class SQRServiceTest_2 {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/data.csv")
+    public void test_2(int x, int y){
+        SQRService service = new SQRService();
+        int actual = service.CalcSqr(x, y);
+        System.out.println(actual);
+    }
+}
